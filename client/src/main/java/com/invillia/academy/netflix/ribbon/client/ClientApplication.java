a2896259322562ef525f4cca6719c15f5db78808
Tuanny <ramostuanny@gmail.com>
2020-07-24 07:26:04 -0300

    change calls to RestTemplate

--- client/src/main/java/com/invillia/academy/netflix/ribbon/client/ClientApplication.java
+++ client/src/main/java/com/invillia/academy/netflix/ribbon/client/ClientApplication.java
@@ -5,24 +5,29 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@EnableFeignClients
 @SpringBootApplication
 public class ClientApplication {
 
 	Logger logger = LoggerFactory.getLogger(ClientApplication.class);
 
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 	@Autowired
-	private HelloWorldClient helloWorldClient;
+	RestTemplate restTemplate;
 
 	@GetMapping("/call-hello")
 	public String sayHelloWorld() {
 		logger.info("chamando hello world API");
-		return helloWorldClient.getHelloFromServer();
+		return this.restTemplate.getForObject("http://localhost:8090/hello", String.class);
 	}
 
 	public static void main(String[] args) {
